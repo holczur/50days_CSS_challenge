286803992ccab5e46ce1551286122cb1550938f9
holczur <holcz.norbert@gmail.com>
2021-05-19 06:32:41 +0200

    day_4: Hidden Search Widget

---
+++ hidden_search_widget/rotating_navigation/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const button = document.querySelector('.btn');
+const search = document.querySelector('.search');
+const input = document.querySelector('.input');
+
+button.addEventListener('click', () => search.classList.toggle('active'));
