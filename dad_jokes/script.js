9301c5351f57bf746042681956c579acd596330c
holczur <holcz.norbert@gmail.com>
2021-05-26 06:10:48 +0200

    day_9: Dad Jokes

---
+++ dad_jokes/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('joke-btn');
+
+jokeBtn.addEventListener('click', generateJoke);
+generateJoke();
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+
+  const res = await fetch('https://icanhazdadjoke.com', config);
+
+  const data = await res.json();
+
+  jokeEl.innerText = data.joke;
+}
