56e0f2c1e13a2a65607e4d7eab9bb7f0cd91dfda
holczur <holcz.norbert@gmail.com>
2021-05-28 15:16:17 +0200

    day_11: faq callapse

---
+++ faq-collapse/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const btns = document.querySelectorAll('.faq-toggle');
+
+btns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.parentNode.classList.toggle('active');
+  });
+});
